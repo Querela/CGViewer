cf2418f07d51b5b71a590feadc84c89c7f6be05d
Querela <ek.92@arcor.de>
2013-07-07 14:05:26 +0200

    gl_Color only

--- Shaders/PhongShader_frag.c
+++ Shaders/PhongShader_frag.c
@@ -1,17 +1,20 @@
 uniform int numLights;
-varying vec3 normal, vertex;
+varying vec3 normal;
+varying vec3 vertex;
 
 void main( )
 {
-    vec4 color = vec4 ( 0.0, 0.0, 0.0, 1.0 );
+    //vec4 color = vec4 ( 0.0, 0.0, 0.0, 1.0 );
+/*
     for ( int l = 0 ; l < numLights ; ++ l )
     {
         vec3 lightPosition = gl_LightSource[ l ].position.xyz;
         vec3 vertexToLight = normalize ( lightPosition - vertex );
+
         color.r += dot ( normal, vertexToLight );
         color.g += dot ( normal, vertexToLight );
         color.b += dot ( normal, vertexToLight );
     }
-    
-    gl_FragColor = color;
+*/
+    gl_FragColor = gl_Color;
 }
--- Shaders/PhongShader_vert.c
+++ Shaders/PhongShader_vert.c
@@ -1,10 +1,13 @@
-varying vec3 normal, vertex;
+varying vec3 normal;
+varying vec3 vertex;
 
 void main( )
 {
-    //gl_Position      = ftransform( );
-    gl_Position        = gl_ModelViewProjectionMatrix * gl_Vertex;
-    normal             = normalize ( gl_NormalMatrix * gl_Normal );
-    vertex             = vec3 ( gl_ModelViewMatrix * gl_Vertex );
+    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+
+    //normal = normalize ( gl_NormalMatrix * gl_Normal );
+    //vertex = vec3 ( gl_ModelViewMatrix * gl_Vertex );
+
+    gl_FrontColor = gl_Color;
 }
 
