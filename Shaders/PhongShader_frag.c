05ec4891eb7797b64a6e234c0c4d4e768f3c687f
Querela <ek.92@arcor.de>
2013-07-10 23:14:48 +0200

    Toon Shader 1

--- Shaders/PhongShader_frag.c
+++ Shaders/PhongShader_frag.c
@@ -1,22 +1,22 @@
 //#version 150
 
 uniform int numLights;
-//uniform sampler2D Texture0;
 uniform sampler2D basemap;
 uniform sampler2D normalmap;
 
 varying vec3 normal;
 varying vec3 vertex;
 
+// constants for toon effects
+const float outline_threshold = 0.2;
+
 void main( )
 {
     vec4 color = vec4 ( 0.0, 0.0, 0.0, 1.0 );
-
-    vec3 invDir = normalize ( -vertex );
-
     color += gl_FrontLightModelProduct.sceneColor;
     color += gl_LightModel.ambient * gl_FrontMaterial.ambient;
-    //color += gl_LightModel.ambient;
+
+    vec3 invDir = normalize ( -vertex );
 
     vec3 N = normal;
 //*
@@ -25,8 +25,8 @@ void main( )
     // heuristic to compute tangent and bitangent for normal map
     vec3 Q1 = dFdx ( vertex );
     vec3 Q2 = dFdy ( vertex );
-    vec2 st1 = dFdx ( vec2 ( gl_TexCoord[0] ) );
-    vec2 st2 = dFdy ( vec2 ( gl_TexCoord[0] ) );
+    vec2 st1 = dFdx ( gl_TexCoord[0].st );
+    vec2 st2 = dFdy ( gl_TexCoord[0].st );
     vec3 tangent = normalize ( Q1 * st2.t - Q2 * st1.t );
     vec3 bitangent = normalize ( -Q1 * st2.s + Q2 * st1.s );
 
@@ -39,9 +39,10 @@ void main( )
     mat3 TBN = mat3 ( tangent, bitangent, normal );
 
     N = N * TBN; // TBN * N if transpose ?
-    //N = normalize ( N );
     N = normalize ( normal + N );
 //*/
+
+    vec4 texture_color = texture2D ( basemap, gl_TexCoord[0].st );
 
     int l = 0;
 //    for ( int l = 0 ; l < numLights ; ++ l )
@@ -50,29 +51,33 @@ void main( )
         vec3 vertexToLight = normalize ( lightPosition - vertex );
         float distanceToLight = length ( lightPosition - vertex );
 
-        // too much lightning ?
-        //vec4 ambient  = gl_LightSource[l].ambient * gl_FrontMaterial.ambient;
-
-        // different sign if texture ?
         vec3 reflected = reflect ( -vertexToLight, N );
 
-        vec4 diffuse  = gl_LightSource[l].diffuse * gl_FrontMaterial.diffuse * 
-                            max ( dot ( N, vertexToLight ), 0.0 );
-        vec4 specular = gl_LightSource[l].specular * gl_FrontMaterial.specular *
-                            pow ( max ( dot ( reflected, invDir ), 0.0 ), gl_FrontMaterial.shininess );
+        // use intensity of diffuse light to create light steps
+        float I_diffuse =  max ( dot ( N, vertexToLight ), 0.0 );
+        if      ( I_diffuse > 0.8 ) color = vec4 ( 1.0 );
+        else if ( I_diffuse > 0.7 ) color = vec4 ( vec3 ( 0.6 ), 1.0 );
+        else if ( I_diffuse > 0.4 ) color = vec4 ( vec3 ( 0.2 ), 1.0 );
+        else if ( I_diffuse > 0.1 ) color = vec4 ( vec3 ( 0.05 ), 1.0 );
 
-        float fatt = gl_LightSource[l].constantAttenuation  +
-                     gl_LightSource[l].linearAttenuation    * distanceToLight +
-                     gl_LightSource[l].quadraticAttenuation * distanceToLight * distanceToLight;
-        if ( fatt != 0.0 )
-            fatt = 1.0 / fatt;
-        else
-            fatt = 1.0;
+//        vec4 specular = gl_LightSource[l].specular * gl_FrontMaterial.specular *
+//                            pow ( max ( dot ( reflected, invDir ), 0.0 ), gl_FrontMaterial.shininess );
+//
+//        float fatt = gl_LightSource[l].constantAttenuation  +
+//                     gl_LightSource[l].linearAttenuation    * distanceToLight +
+//                     gl_LightSource[l].quadraticAttenuation * distanceToLight * distanceToLight;
+//        if ( fatt != 0.0 )
+//            fatt = 1.0 / fatt;
+//        else
+//            fatt = 1.0;
 
-        //color += ambient + diffuse + specular;
-        color += fatt * ( diffuse + specular );
 //    }
 
-    gl_FragColor = color * texture2D ( basemap, gl_TexCoord[0].st );
+    // use the texture color to get some other shades than black and white
+    gl_FragColor = color * smoothstep ( 0.3, 0.6, texture_color );
+
+    // outline
+    if ( dot ( invDir, N ) < outline_threshold )
+        gl_FragColor = vec4 ( 0.0, 0.0, 0.0, 1.0 );
 }
 
